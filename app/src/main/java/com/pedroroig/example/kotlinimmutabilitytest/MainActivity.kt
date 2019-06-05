b610fa1a2cdfb4a6cb910ce57ad4448e02dd5acd
Pedro Roig <pedro.roiggimenez@soprasteria.com>
2019-06-05 17:27:58 +0200

    Exercise 3 solution

--- app/src/main/java/com/pedroroig/example/kotlinimmutabilitytest/MainActivity.kt
+++ app/src/main/java/com/pedroroig/example/kotlinimmutabilitytest/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // TODO Modify person object François to be Pepe, without making name var nor using Person's constructor
-//        person.name = "Pepe"
+        person = person.copy(name = "Pepe")
 
         // Should show initial value "François"
         tvHello.text = person.name
